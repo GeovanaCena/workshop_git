df45c459df279158255973a10beea197eccd6520
GeovanaCena <geovana.cena@icloud.com>
2026-06-03 14:32:03 -0400

    Initial Commit

---
+++ workshop_git/workshop_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshop_gitApp.swift
+//  workshop_git
+//
+//  Created by Academy on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct workshop_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
